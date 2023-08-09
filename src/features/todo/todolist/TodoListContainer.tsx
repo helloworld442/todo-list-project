81a2073cbb97e6014ce42ac6495b8f87d3806386
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-10 02:15:43 +0900

    fix : fix the todo list component naming #7

--- src/features/todo/todolist/TodoListContainer.tsx
+++ src/features/todo/todolist/TodoListContainer.tsx
@@ -1,14 +1,14 @@
-import TodoList from "./TodoListComponent";
+import TodoListComponent from "./TodoListComponent";
 
 const TodoListContainer = () => {
   return (
-    <TodoList>
-      <TodoList.Title />
-      <TodoList.Toggle />
-      <TodoList.Menu>
-        <TodoList.Item />
-      </TodoList.Menu>
-    </TodoList>
+    <TodoListComponent>
+      <TodoListComponent.Title />
+      <TodoListComponent.Toggle />
+      <TodoListComponent.Menu>
+        <TodoListComponent.Item />
+      </TodoListComponent.Menu>
+    </TodoListComponent>
   );
 };
 
