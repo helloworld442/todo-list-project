763398666c73248ea19102b36ccf819e5e9c2ec0
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-11 07:17:18 +0900

    feat : plus the root style to GlobalStyle file

--- src/utils/GlobalStyle.tsx
+++ src/utils/GlobalStyle.tsx
@@ -4,6 +4,15 @@ import { normalize } from "styled-normalize";
 const GlobalStyle = createGlobalStyle`
     ${normalize};
 
+    :root {
+        font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+        font-synthesis: none;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-text-size-adjust: 100%;
+    }
+
     body {
         padding: 0;
         margin: 0;
