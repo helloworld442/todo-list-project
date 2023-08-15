af7f00c9f4f7afdf49945127c4cfdf22c6f319af
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-15 18:11:38 +0900

    feat : feat todo head menu logic #6

--- src/features/todo/component/TodoHead.jsx
+++ src/features/todo/component/TodoHead.jsx
@@ -1,15 +1,32 @@
 import "./TodoHead.scss";
+import { useState } from "react";
 
 const TodoHead = () => {
+  const dates = [
+    { id: 0, day: "S", num: 6 },
+    { id: 1, day: "M", num: 7 },
+    { id: 2, day: "T", num: 8 },
+    { id: 3, day: "W", num: 9 },
+    { id: 4, day: "T", num: 10 },
+    { id: 5, day: "F", num: 11 },
+    { id: 6, day: "S", num: 12 },
+  ];
+  const [isOpen, setIsOpen] = useState(0);
+
+  const onClickItem = (index) => setIsOpen(index);
+
   return (
     <div className="todohead-col">
       <h3 className="todohead-title small">Friday,May 11</h3>
       <h4 className="todohead-title large">To-Do List</h4>
       <ul className="todohead-date-list">
-        <li className="todohead-date-item">
-          <span className="date-day">6</span>
-          <span className="date-num">5</span>
-        </li>
+        {dates.map((date) => (
+          <li key={date.id} className="todohead-date-item" onClick={() => onClickItem(date.id)}>
+            <span className="date-day">{date.day}</span>
+            <span className="date-num">{date.num}</span>
+            {date.id === isOpen ? <span className="date-icon"></span> : null}
+          </li>
+        ))}
       </ul>
     </div>
   );
