ccfb542e81ce900947972ad18af2779280bf72a1
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-15 20:10:52 +0900

    feat : add write route in router page #10

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Write from "../pages/Write";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/write" element={<Write />} />
       </Routes>
     </BrowserRouter>
   );
