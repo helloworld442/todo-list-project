370f7b8e753f1342d2879b1fc512d2284f32244f
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-15 20:49:11 +0900

    feat : feat chart and login and register page #17

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { Footer, Header, Layout } from "../features/ui";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Header />
+      <Footer />
+    </Layout>
+  );
+};
+
+export default Home;
