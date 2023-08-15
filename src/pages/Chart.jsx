634bce8d4d35d2b2b543cd494c402771cbffb36f
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-15 22:46:24 +0900

    feat : feat todo chart style #22

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -1,9 +1,11 @@
 import { Footer, Header, Layout } from "../features/ui";
+import { TodoChart } from "../features/todo/component";
 
 const Chart = () => {
   return (
     <Layout>
       <Header />
+      <TodoChart />
       <Footer />
     </Layout>
   );
