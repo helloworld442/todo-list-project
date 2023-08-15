32a960e19200b1cd9f2166c5660126eb87bc9df5
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-16 06:37:18 +0900

    feat : feat input component js Doc #36

--- src/features/ui/input/Input.jsx
+++ src/features/ui/input/Input.jsx
@@ -9,6 +9,14 @@ const inputDict = {
   password: "PASSWORD",
 };
 
+/**
+ * @param {string} type 인풋 타입
+ * @param {string} name 인풋 이름
+ * @param {string} value 인풋 값
+ * @param {string} error 인풋의 값과 관련된 유효성 검사 내용
+ * @param {Function} onChange 인풋의 값이 바뀔 때 호출되는 함수
+ */
+
 const Input = memo(({ type, name, value, error, onChange }) => {
   return (
     <div className="input-box">
