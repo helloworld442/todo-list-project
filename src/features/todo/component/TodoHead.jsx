950ca61cd11b6cc14e25e31a0c59a294d7c83fab
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-15 18:04:27 +0900

    feat : feat todo head date menu style #5

--- src/features/todo/component/TodoHead.jsx
+++ src/features/todo/component/TodoHead.jsx
@@ -6,7 +6,10 @@ const TodoHead = () => {
       <h3 className="todohead-title small">Friday,May 11</h3>
       <h4 className="todohead-title large">To-Do List</h4>
       <ul className="todohead-date-list">
-        <li className="todohead-date-item"></li>
+        <li className="todohead-date-item">
+          <span className="date-day">6</span>
+          <span className="date-num">5</span>
+        </li>
       </ul>
     </div>
   );
