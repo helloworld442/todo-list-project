e7b2cc4ef0e4d2f3f8bb189176a795773d09ba25
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-16 10:10:29 +0900

    fix : fix todo item style and file structure #45 #46

--- src/features/todo/component/TodoList.jsx
+++ src/features/todo/component/TodoList.jsx
@@ -1,26 +1,11 @@
 import "./TodoList.scss";
-import classNames from "classnames";
-import { useState } from "react";
+import TodoItem from "./TodoItem";
 
 const TodoList = () => {
   return (
     <ul className="todolist">
       <TodoItem />
     </ul>
-  );
-};
-
-const TodoItem = () => {
-  const [active, setActive] = useState(false);
-
-  const onToggleCheckBox = () => setActive(!active);
-
-  return (
-    <li className="todoitem">
-      <span className="todoitem-icon"></span>
-      <span className={classNames("todoitem-text", { active })}>asdf</span>
-      <input className="todoitem-checkbox" type="checkbox" onClick={onToggleCheckBox} />
-    </li>
   );
 };
 
